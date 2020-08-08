398f7a206d2054af05ab37be99c0cde1e8fa4150
Ben Nuttall <ben@bennuttall.com>
2020-08-08 17:16:37 +0100

    Add 'new daily record' for time saved

--- src/daily.py
+++ src/daily.py
@@ -3,13 +3,14 @@ from db import PiWheelsDatabase
 
 logfile('/var/log/piwheels-twitter/daily.log', maxBytes=1e6)
 logger.info('START')
-record_file = '/home/piwheels/daily_record.txt'
+record_downloads_file = '/home/piwheels/daily_downloads_record.txt'
+record_time_saved_file = '/home/piwheels/daily_time_saved_record.txt'
 
-def update_record(downloads):
+def update_record(record_file, data):
     with open(record_file, 'w') as f:
-        f.write(str(downloads))
+        f.write(str(data))
 
-def get_record():
+def get_record(record_file):
     with open(record_file) as f:
         return int(f.read().strip())
 
@@ -25,20 +26,23 @@ downloads = db.count_downloads_yesterday()
 logger.debug('downloads: {}'.format(downloads))
 time_saved = db.get_time_saved_yesterday()
 logger.debug('time saved: {}'.format(time_saved))
-record = get_record()
 
-if downloads > record:
+record_downloads = get_record(record_downloads_file)
+downloads_record = ''
+if downloads > record_downloads:
     downloads_record = ' (a new daily record)'
-    update_record(downloads)
-else:
-    downloads_record = ''
+    update_record(record_downloads_file, downloads)
 
+record_time_saved = get_record(record_time_saved_file)
 time_saved_record = ''
+if time_saved > record_time_saved:
+    time_saved_record = ' (a new daily record)'
+    update_record(record_time_saved_file, time_saved)
 
 tweet = ('Yesterday, {:,} packages were downloaded from piwheels.org{}, '
-         'saving users over {} days of build time').format(downloads,
+         'saving users over {} days{} of build time').format(downloads,
                                                            downloads_record,
-                                                           time_saved.days,
+                                                           time_saved,
                                                            time_saved_record)
 try:
     logger.info('Tweeting: {}'.format(tweet))
